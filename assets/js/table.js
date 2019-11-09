ff0fa648cd8ad93819fb1ff254bf28dde7e7ae1d
DanisAvko <valievdanis@bk.ru>
2019-11-09 12:05:26 +0300

    new

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,9 +1,9 @@
-var tableLine=new Map([
-    ["5",{id:"5",name:"AIphone 5",cost:"400",count:"5"}],
-    ["2",{id:"2",name:"DXBOX",cost:"300",count:"7"}],
-    ["3",{id:"3",name:"CPS",cost:"302",count:"3"}]
-]);
-let numSort= (a, b)=>a-b;
+var tableLine={
+    "5":{id:"5",name:"AIphone",cost:"400",count:"5"},
+    "2":{id:"2",name:"DXBOX",cost:"300",count:"7"},
+    "3":{id:"3",name:"CPS",cost:"302",count:"3"}
+};
+var numSort= (a, b)=>a-b;
 function strSort(a,b) {
     if(a>b){
         return 1
@@ -15,33 +15,39 @@ function strSort(a,b) {
 }
 
 $(document).ready(function() {
-    tableLine.forEach(function (item) {
+    $.each(tableLine,function (key,value) {
         $('.tbody').append(`<tr >\n +
-                            <td>${item.id}</td>\n" +
-                            <td>${item.name}</td>\n" +
-                            <td>${item.cost}</td>\n" +
-                            <td>${item.count}</td>\n" +
-                            <td id="${item.id}"><button type="button"  class="close" >X</button></td>\n" +
+                            <td >${value.id}</td>\n" +
+                            <td>${value.name}</td>\n" +
+                            <td>${value.cost}</td>\n" +
+                            <td>${value.count}</td>\n" +
+                            <td id="${value.id}"><button type="button"  class="close" >x</button></td>\n" +
                        </tr>`);
     });
 
     let trs =$('#table>tbody>tr');
     var k=0;
     $("#id").click(function () {
+        $.each(Array($('#namesort'),$('#costsort'),$('#countsort')),
+            function (index,value) {
+                if(value.hasClass("fa-sort-up")) value.toggleClass("fa-sort-up fa-sort");
+                if(value.hasClass("fa-sort-down")) value.toggleClass("fa-sort-down fa-sort");
+            }
+        );
         let sorted;
         if(k===0) {
             k++;
-            $("#id>#idsort").toggleClass("fa-sort fa-sort-up");
+            $("#idsort").toggleClass("fa-sort fa-sort-up");
          sorted=[...trs].sort((a,b)=>numSort(a.children[0].innerHTML,b.children[0].innerHTML));
         }
         else if(k===1){
             k++;
-            $("#id>#idsort").toggleClass("fa-sort-up fa-sort-down");
+            $("#idsort").toggleClass("fa-sort-up fa-sort-down");
          sorted=[...trs].sort((a,b)=>numSort(b.children[0].innerHTML,a.children[0].innerHTML));
         }
         else {
             k=0;
-            $("#id>#idsort").toggleClass("fa-sort-down fa-sort");
+            $("#idsort").toggleClass("fa-sort-down fa-sort");
             sorted=[...trs]
         }
         $('.tbody').text('');
@@ -53,20 +59,26 @@ $(document).ready(function() {
     var n=0;
 
     $("#name").click(function () {
-        var sorted;
+        $.each(Array($('#idsort'),$('#costsort'),$('#countsort')),
+            function (index,value) {
+                if(value.hasClass("fa-sort-up")) value.toggleClass("fa-sort-up fa-sort");
+                if(value.hasClass("fa-sort-down")) value.toggleClass("fa-sort-down fa-sort");
+            }
+        );
+        let sorted;
         if(n===0) {
             n++;
-            $("#name>#namesort").toggleClass("fa-sort fa-sort-up");
+            $("#namesort").toggleClass("fa-sort fa-sort-up");
             sorted=[...trs].sort((a,b)=>strSort(a.children[1].innerHTML,b.children[1].innerHTML));
         }
         else if(n===1){
             n++;
-            $("#name>#namesort").toggleClass("fa-sort-up fa-sort-down");
+            $("#namesort").toggleClass("fa-sort-up fa-sort-down");
             sorted=[...trs].sort((a,b)=>strSort(b.children[1].innerHTML,a.children[1].innerHTML));
         }
         else {
             n=0;
-            $("#name>#namesort").toggleClass("fa-sort-down fa-sort");
+            $("#namesort").toggleClass("fa-sort-down fa-sort");
             sorted=[...trs]
         }
         $('.tbody').text('');
@@ -77,20 +89,26 @@ $(document).ready(function() {
     });
     var l=0;
     $("#cost").click(function () {
+        $.each(Array($('#idsort'),$('#namesort'),$('#countsort')),
+            function (index,value) {
+                if(value.hasClass("fa-sort-up")) value.toggleClass("fa-sort-up fa-sort");
+                if(value.hasClass("fa-sort-down")) value.toggleClass("fa-sort-down fa-sort");
+            }
+        );
         let sorted;
         if(l===0) {
             l++;
-            $("#cost>#costsort").toggleClass("fa-sort fa-sort-up");
+            $("#costsort").toggleClass("fa-sort fa-sort-up");
             sorted=[...trs].sort((a,b)=>numSort(a.children[2].innerHTML,b.children[2].innerHTML));
         }
         else if(l===1){
             l++;
-            $("#cost>#costsort").toggleClass("fa-sort-up fa-sort-down");
+            $("#costsort").toggleClass("fa-sort-up fa-sort-down");
             sorted=[...trs].sort((a,b)=>numSort(b.children[2].innerHTML,a.children[2].innerHTML));
         }
         else {
             l=0;
-            $("#cost>#costsort").toggleClass("fa-sort-down fa-sort");
+            $("#costsort").toggleClass("fa-sort-down fa-sort");
             sorted=[...trs]
         }
         $('.tbody').text('');
@@ -101,20 +119,26 @@ $(document).ready(function() {
     });
     var t=0;
     $("#count").click(function () {
+        $.each(Array($('#idsort'),$('#namesort'),$('#costsort')),
+            function (index,value) {
+                if(value.hasClass("fa-sort-up")) value.toggleClass("fa-sort-up fa-sort");
+                if(value.hasClass("fa-sort-down")) value.toggleClass("fa-sort-down fa-sort");
+            }
+        );
         let sorted;
         if(t===0) {
             t++;
-            $("#count>#countsort").toggleClass("fa-sort fa-sort-up");
+            $("#countsort").toggleClass("fa-sort fa-sort-up");
             sorted=[...trs].sort((a,b)=>numSort(a.children[3].innerHTML,b.children[3].innerHTML));
         }
         else if(t===1){
             t++;
-            $("#count>#countsort").toggleClass("fa-sort-up fa-sort-down");
+            $("#countsort").toggleClass("fa-sort-up fa-sort-down");
             sorted=[...trs].sort((a,b)=>numSort(b.children[3].innerHTML,a.children[3].innerHTML));
         }
         else {
             t=0;
-            $("#count>#countsort").toggleClass("fa-sort-down fa-sort");
+            $("#countsort").toggleClass("fa-sort-down fa-sort");
             sorted=[...trs]
         }
         $('.tbody').text('');
@@ -124,22 +148,22 @@ $(document).ready(function() {
         }
     });
     $("#add").click(function () {
-        let dat=$("#dataId");
+        let dataId=$("#dataId");
         let item={
             id:$('#dataId').val(),
             name:$('#dataName').val(),
             cost:$('#dataCost').val(),
             count:$('#dataCount').val()
         };
-        tableLine.set(dat.val(),item);
+        tableLine[dataId.val()]=item;
         $(".tbody").append(`<tr >\n +
         <td>${item.id}</td>\n" +
         <td>${item.name}</td>\n" +
         <td>${item.cost}</td>\n" +
         <td>${item.count}</td>\n" +
-        <td id="${item.id}"><button type="button"  class="close" >X</button></td>\n" +
+        <td id="${item.id}"><button type="button"  class="close" >x</button></td>\n" +
         </tr>`);
-        dat.val("");
+        dataId.val("");
         $("#dataName").val("");
         $("#dataCost").val("");
         $("#dataCount").val("");
@@ -156,10 +180,7 @@ $(document).ready(function() {
     $('table').delegate('button','click',function(e) {
         let trId=e.currentTarget.parentElement.id;
         $(`#${trId}`).parent().remove();
-        tableLine.delete(trId);
-        console.log("12">="1");
-        console.log("12">="9");
-        console.log("10"-"9");
+        delete tableLine[trId];
         trs =$('#table>tbody>tr');
     });
 });
