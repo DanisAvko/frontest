0d05fce98367c5d825a845d43261710c74f36cf7
DanisAvko <valievdanis@bk.ru>
2019-11-09 16:27:57 +0300

    new

--- assets/js/table.js
+++ assets/js/table.js
@@ -3,20 +3,28 @@ var tableLine = Array(
     {id: "5", name: "AIphone", cost: "400", count: "5"},
     {id: "2", name: "DXBOX", cost: "300", count: "7"},
     {id: "3", name: "CPS", cost: "302", count: "3"},
-    {id: "100", name: "CPS", cost: "302", count: "3"}
+    {id: "100", name: "CPS", cost: "302", count: "3"},
+    {id: "101", name: "CPS", cost: "302", count: "3"},
+    {id: "51", name: "AIphone", cost: "400", count: "5"},
+    {id: "21", name: "DXBOX", cost: "300", count: "7"},
+    {id: "31", name: "CPS", cost: "302", count: "3"},
+    {id: "1001", name: "CPS", cost: "302", count: "3"},
+    {id: "102", name: "CPS", cost: "302", count: "3"},
+    {id: "52", name: "AIphone", cost: "400", count: "5"},
+    {id: "22", name: "DXBOX", cost: "300", count: "7"},
+    {id: "32", name: "CPS", cost: "302", count: "3"},
+    {id: "102", name: "CPS", cost: "302", count: "3"}
 );
-var numSort = (a, b) => a - b;
 
-function strSort(a, b) {
-    if (a > b) {
-        return 1
-    } else if (a < b) {
-        return -1
-    } else return 0
+function tSort(a, b, index) {
+    if (index === "name") {
+        if (a > b) return 1
+        else if (a < b) return -1
+        else return 0
+    } else return b - a
 }
 
 $(document).ready(function () {
-
     $.each(tableLine,
         function (index, value) {
             $('.tbody').append(`<tr >\n +
@@ -24,7 +32,7 @@ $(document).ready(function () {
                              <td>${value.name}</td>\n" +
                              <td>${value.cost}</td>\n" +
                              <td>${value.count}</td>\n" +
-                             <td id="${value.id}"><button type="button"  class="close" >x</button></td>\n" +
+                             <td id="${value.id}"><button type="button"  class="close" ><i class="fa fa-times" aria-hidden="true"></i></button></td>\n" +
                            </tr>`);
         }
     );
@@ -34,36 +42,24 @@ $(document).ready(function () {
         let sorted;
         let currenChildId = e.currentTarget.children[0].id;
         let currentChildClassName = e.currentTarget.children[0].className.split(' ')[1];
-        let columnNumber = [...$('thead tr').children()].indexOf(e.currentTarget);
+        let columnNumber = e.currentTarget.cellIndex;
         let arr = Array($('#idsort'), $('#namesort'), $('#costsort'), $('#countsort'));
-        arr.splice(columnNumber, 1)
+        arr.splice(columnNumber, 1);
         $.each(arr, function (index, value) {
             if (value.hasClass("fa-sort-up")) value.toggleClass("fa-sort-up fa-sort");
             if (value.hasClass("fa-sort-down")) value.toggleClass("fa-sort-down fa-sort");
         });
-        if (e.currentTarget.id === "name") {
-            if (currentChildClassName === "fa-sort") {
-                sorted = [...trs].sort((a, b) => strSort(b.children[1].innerHTML, a.children[1].innerHTML));
-                $(`#${currenChildId}`).toggleClass("fa-sort fa-sort-up")
-            } else if (currentChildClassName === "fa-sort-up") {
-                sorted = [...trs].sort((a, b) => strSort(a.children[1].innerHTML, b.children[1].innerHTML));
-                $(`#${currenChildId}`).toggleClass("fa-sort-up fa-sort-down")
-            } else {
-                sorted = [...trs]
-                $(`#${currenChildId}`).toggleClass("fa-sort-down fa-sort")
-            }
+        if (currentChildClassName === "fa-sort") {
+            sorted = [...trs].sort((a, b) => tSort(b.children[columnNumber].innerHTML, a.children[columnNumber].innerHTML, e.currentTarget.id));
+            $(`#${currenChildId}`).toggleClass("fa-sort fa-sort-up")
+        } else if (currentChildClassName === "fa-sort-up") {
+            sorted = [...trs].sort((a, b) => tSort(a.children[columnNumber].innerHTML, b.children[columnNumber].innerHTML, e.currentTarget.id));
+            $(`#${currenChildId}`).toggleClass("fa-sort-up fa-sort-down")
         } else {
-            if (currentChildClassName === "fa-sort") {
-                sorted = [...trs].sort((a, b) => numSort(a.children[columnNumber].innerHTML, b.children[columnNumber].innerHTML));
-                $(`#${currenChildId}`).toggleClass("fa-sort fa-sort-up")
-            } else if (currentChildClassName === "fa-sort-up") {
-                sorted = [...trs].sort((a, b) => numSort(b.children[columnNumber].innerHTML, a.children[columnNumber].innerHTML));
-                $(`#${currenChildId}`).toggleClass("fa-sort-up fa-sort-down")
-            } else {
-                sorted = [...trs];
-                $(`#${currenChildId}`).toggleClass("fa-sort-down fa-sort")
-            }
+            sorted = [...trs];
+            $(`#${currenChildId}`).toggleClass("fa-sort-down fa-sort")
         }
+
         $('.tbody').text('');
         for (let s of sorted) {
             $(".tbody").append(s);
@@ -91,7 +87,7 @@ $(document).ready(function () {
         <td>${item.name}</td>\n" +
         <td>${item.cost}</td>\n" +
         <td>${item.count}</td>\n" +
-        <td id="${item.id}"><button type="button"  class="close" >x</button></td>\n" +
+        <td id="${item.id}"><button type="button"  class="close" ><i class="fa fa-times" aria-hidden="true"></i></button></td>\n" +
         </tr>`);
             dataId.val("");
             $("#dataName").val("");
